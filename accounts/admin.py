0381a14d0a3b17f9a62e32f81ec71ec8db072f4f
Chrisagbara1 <chrisagbara2023@mail.com>
2025-08-31 04:48:45 +0100

    custom user model set in model.py in accounts and admin.py updated

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ("Extra", {"fields": ("is_teacher", "bio", "profile_picture")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Extra", {"fields": ("is_teacher",)}),
+    )
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    """
+    Custom user with an is_teacher flag.
+    Only signed-up users are allowed; they can be teachers or learners.
+    """
+    is_teacher = models.BooleanField(default=False)
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="profiles/", blank=True, null=True)
+
+    def __str__(self):
+        return self.username
